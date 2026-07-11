2b4b22b1054f7fe6e53ecc91aea9207f354cae07
shubham0333 <shindeshubham0333@gmail.com>
2026-07-11 17:39:13 +0530

    Add requirements.txt

--- backend/main.py
+++ backend/main.py
@@ -8,9 +8,13 @@ from fastapi.middleware.cors import CORSMiddleware
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+    "https://taskmanager-1ulsy8iza-shubham0333.vercel.app",
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
